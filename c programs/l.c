bece7e187a3860a22f7a33a20b144b2c8fed6efe
Aarush289 <cs24b064@smail.iitm.ac.in>
2025-09-17 17:44:15 +0530

    commit

--- c programs/l.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(){
-    int array[3]={12,3,23};
-    qsort(array);
-    printf("%d %d\n",array[0],array[1]);
-}
--- c programs/q1.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    int x1,x2;
-    scanf("%d%d",&x1,&x2);
-    printf("%d",(6-x1-x2));
-    return 0;
-}
